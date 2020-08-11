21e9883df484a3163bc1659c0a71247296d1624f
LJSprojects <69403515+LJSprojects@users.noreply.github.com>
2020-08-11 14:27:56 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,8 @@
+function toCalendar(){
+    console.log("go to calendar page")
+}
 
+function sendMail(){
+    window.location.href = "mailto:INFO@ljsprojects.com"
+    console.log("send an email")
+}
