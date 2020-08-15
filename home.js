26d14ab9ce3fec066e5ece99bdf3bf01817069b2
LJSprojects <69403515+LJSprojects@users.noreply.github.com>
2020-08-15 12:01:26 -0500

    Update home.js

--- home.js
+++ home.js
@@ -6,3 +6,18 @@ function sendMail(){
     window.location.href = "mailto:INFO@ljsprojects.com"
     console.log("send an email")
 }
+
+function sizeColumns(){
+    var width = screen.width - 300;
+
+    console.log(width);
+    document.getElementById("rightCol").style.width = width + "px" ;
+    document.getElementById("background").style.width = width + "px";
+}
+
+function scrollCheck(){
+    var rect = document.getElementById("rightCol").getBoundingClientRect();
+    var left = rect.left;
+
+    // console.log(left);
+}
